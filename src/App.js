7a6bd35c5057bb66c281587ea7f0ef4955b5483a
sky121016 <sky121016@naver.com>
2023-12-30 20:15:08 +0900

    add netlify.toml

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Shopping from './pages/Shopping';
-import Trainer from './pages/Trainer';
-import Communitypage from './pages/Communitypage';
-import Mypage from './pages/Mypage'
-import ProductDetail from './pages/ProductDetail';
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Shopping from "./pages/Shopping";
+import Trainer from "./pages/Trainer";
+import Communitypage from "./pages/Communitypage";
+import Mypage from "./pages/Mypage";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/shopping' element={<Shopping/>}/>
-          <Route path='/trainer' element={<Trainer/>}/>
-          <Route path='/communitypage' element={<Communitypage/>}/>
-          <Route path='/mypage' element={<Mypage/>}/>
-          <Route path='/shopping/product/:title' element={<ProductDetail/>}/>
-
-
-
+          <Route path="/" element={<Home />} />
+          <Route path="/shopping" element={<Shopping />} />
+          <Route path="/trainer" element={<Trainer />} />
+          <Route path="/communitypage" element={<Communitypage />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/shopping/product/:title" element={<ProductDetail />} />
         </Routes>
       </BrowserRouter>
-  
-      <Footer/>
+
+      <Footer />
     </div>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,47 +1,51 @@
 import React from "react";
-import cart from "../assets/icons/cart.svg"
+import cart from "../assets/icons/cart.svg";
 import { useNavigate } from "react-router-dom";
 
 const Product = (props) => {
-    const {id, title, originPrice, salePrice, percentage, image, tag} = props.product;
+  const { id, title, originPrice, salePrice, percentage, image, tag } =
+    props.product;
 
-    const navigate = useNavigate();
-    const onClickProduct = () => {
-        navigate(`/shopping/product/${title}`,{
-            state:props.product
-        })
-    }
+  const navigate = useNavigate();
+  const onClickProduct = () => {
+    navigate(`/shopping/product/${title}`, {
+      state: props.product,
+    });
+  };
 
-    return (
-        <div className="product-item" key={id} onClick={onClickProduct}>
-            <div className="product-img">
-                <img src={image} alt="이미지" />
-                <div className="cart">
-                    <img src={cart} alt="장바구니" />
-                </div>
-            </div>
-
-            {tag!="" && (<div className="product-tag">
-                {tag.map((t, index)=>(
-                    <span key={index}>{t.label}</span>
-                ))}
-            </div>)}
-
-            <p className="product-title">{title}</p>
-            {salePrice ? (
-                <div className="price">
-                    <p className="originPrice strikethrough">
-                        {originPrice}
-                    </p>
-                    <p><span>{percentage} </span>{salePrice}</p>
-                </div>
-            ) : (
-                <div className="price">
-                    <p>{originPrice}</p>
-                </div>
-            )}
+  return (
+    <div className="product-item" key={id} onClick={onClickProduct}>
+      <div className="product-img">
+        <img src={image} alt="이미지" />
+        <div className="cart">
+          <img src={cart} alt="장바구니" />
         </div>
-    );
+      </div>
+
+      {tag != "" && (
+        <div className="product-tag">
+          {tag.map((t, index) => (
+            <span key={index}>{t.label}</span>
+          ))}
+        </div>
+      )}
+
+      <p className="product-title">{title}</p>
+      {salePrice ? (
+        <div className="price">
+          <p className="originPrice strikethrough">{originPrice}</p>
+          <p>
+            <span>{percentage} </span>
+            {salePrice}
+          </p>
+        </div>
+      ) : (
+        <div className="price">
+          <p>{originPrice}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Product;
